3e72561ec921b4774fdbd7358370eba07cf0a3dd
cisco <sovisrushain@gmail.com>
2021-12-13 16:06:53 +0530

    PostConstruct and PreDestroy

---
+++ bean-lifecycle/src/main/java/com/cisco/spring/bean/SpringBean2.java
@@ -0,0 +1,20 @@
+package com.cisco.spring.bean;
+
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+@Component
+public class SpringBean2 {
+
+    @PostConstruct
+    public void init(){
+        System.out.println("SpringBean2 - Initialization");
+    }
+
+    @PreDestroy
+    public void destroy(){
+        System.out.println("SpringBean2 - Just before destroy");
+    }
+}
